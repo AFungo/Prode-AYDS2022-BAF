d2a782d5f04ad0f896c3f50eae92d2b1057e56e0
AFungo <augustofungo@gmail.com>
2022-05-09 17:04:31 -0300

    I update predicttion class. Ref #3

--- src/db/migrate/20220505134412_create_prediction.rb
+++ src/db/migrate/20220505134412_create_prediction.rb
@@ -1,7 +1,12 @@
 class CreatePrediction < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-        t.integer :Team1_goals
-        t.integer :Team2_goals
+      t.references :match
+
+      t.integer :Team1_goals
+      t.integer :Team2_goals
+
+      t.timestamps
+    end
   end
 end
--- src/models/Prediction.rb
+++ src/models/Prediction.rb
@@ -1,6 +1,8 @@
  require 'active_record'
 
 class Prediction < ActiveRecord::Base
+	belongs_to :Match
+	belongs_to :Gambler
 end
 
 
