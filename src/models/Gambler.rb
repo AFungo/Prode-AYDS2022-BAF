bd63fd832dd19af7de2001c1367ee778f2e98441
AFungo <augustofungo@gmail.com>
2022-05-20 11:35:12 -0300

    I add validates to models gambler

--- src/models/Gambler.rb
+++ src/models/Gambler.rb
@@ -4,7 +4,10 @@ require 'bcrypt'
 class Gambler < ActiveRecord::Base
 	include BCrypt
 	has_many :prediction
-  
+  validates  :name, presence: true
+  validates :lastname, presence: true
+  validates :Email, presence: true
+  validates :Email, uniqueness: true
   def password
     @password ||= Password.new(password_digest)
   end
@@ -14,7 +17,11 @@ class Gambler < ActiveRecord::Base
     self.password_digest = @password
   end
 
-end
+  def create_prediction=(match,t1Goals,t2Goals)
+    Prediction.new(team1_goals: t1Goals, team2_goals: t2Goals, match: match, gambler: self) 
+  end
+
+  end
 
 
 
