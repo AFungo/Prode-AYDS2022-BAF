6b42426351c94cfb319b6c9be61adf1edd1e6d58
AFungo <augustofungo@gmail.com>
2022-09-13 09:30:20 -0300

    I add test for all models

--- src/spec/test/test_Match_spec.rb
+++ src/spec/test/test_Match_spec.rb
@@ -2,16 +2,18 @@ require_relative '../../models/init.rb'
 
 #Test
 describe 'Match' do
-  describe "valid" do
-        describe 'when there is valid' do
-            let!(:team_1) {Team.create(name: 'nigeria')}
-            let!(:team_2) {Team.create(name: 'argentina')}
-            let!(:team_3) {Team.create(name: 'italia')}
-            let!(:team_4) {Team.create(name: 'irak')}
+    describe "valid" do
+        
+        let!(:team_1) {Team.create(name: 'nigeria')}
+        let!(:team_2) {Team.create(name: 'argentina')}
+        let!(:team_3) {Team.create(name: 'italia')}
+        let!(:team_4) {Team.create(name: 'irak')}
 
-            let!(:date){DateTime.new(2022,12,3,12,10)}
-            let!(:valid_match) {Match.create(local: team_1, visitor: team_2, round: 4, datetime: date)}
-            
+        let!(:date){DateTime.new(2022,12,3,12,10)}
+        let!(:valid_match) {Match.create(local: team_1, visitor: team_2, round: 4, datetime: date)}    
+    
+        describe 'when there is valid' do
+                
             it "Add a new match in db" do
                 match = Match.new(local_id: 3, visitor_id: 4, round:2, datetime: date)
                 valid = match.valid?
@@ -24,9 +26,24 @@ describe 'Match' do
                 expect(match.valid?).to eq(true)
             end
             
+        end
+
+        describe 'when there is invalid' do
             it "Add duplicated match in db" do
                 inv_match = Match.new(local_id: 1, visitor_id: 2, round: 4, datetime: date)
                 expect(inv_match.valid?).to eq(false)
+            end
+
+            it "Add match without round" do
+                match = Match.new(local_id: 1, visitor_id: 4, datetime: date)
+                valid = match.valid?
+                expect(valid).to eq(false)
+            end
+
+            it "Add match without one team" do
+                match = Match.new(local_id: 1,  round: 3, datetime: date)
+                valid = match.valid?
+                expect(valid).to eq(false)
             end
         end
     end
