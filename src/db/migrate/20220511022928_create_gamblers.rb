4fcb7f60fe185e13f2f9ea3d8e31e9909a009b00
Valeria-Bettoni <valebettoni.vb@gmail.com>
2022-10-03 11:24:54 -0300

    https://www.pivotaltracker.com/story/show/183187790

--- src/db/migrate/20220511022928_create_gamblers.rb
+++ src/db/migrate/20220511022928_create_gamblers.rb
@@ -4,7 +4,7 @@ class CreateGamblers < ActiveRecord::Migration[7.0]
         t.string :name
         t.string :Email
         t.integer :Total_score, default: 0
-        t.float : Statistical_data, default: 0
+        t.float :Statistical_data, default: 0
         t.references :predictions
         t.boolean :Admin, default: false
  
