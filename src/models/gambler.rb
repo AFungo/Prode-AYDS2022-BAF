5ad820a4d391553606725cc6b45649bad1601635
Santiago Alfonso <santialfonso2500@gmail.com>
2022-10-13 16:19:18 -0300

    Ofensas corregidas en gambler

--- src/models/gambler.rb
+++ src/models/gambler.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require 'active_record'
 require 'bcrypt'
 
+# Clase que representa a los usuarios.
 class Gambler < ActiveRecord::Base
   include BCrypt
   # Un jugador hace muchas predicciones
@@ -24,8 +27,8 @@ class Gambler < ActiveRecord::Base
   end
 
   # Crear una prediccion
-  def create_prediction(match, t1Goals, t2Goals)
-    Prediction.new(team1_goals: t1Goals, team2_goals: t2Goals, match: match, gambler: self)
+  def create_prediction(match, t1goals, t2goals)
+    Prediction.new(team1_goals: t1goals, team2_goals: t2goals, match: match, gambler: self)
   end
 
   def statistics
@@ -33,20 +36,20 @@ class Gambler < ActiveRecord::Base
     # ¿Como traer la prediccion y el resultado del partido correspondiente de un cierto usuario?
     cant_aciertos = 0
     # Todas las predicciones de un cierto usuario
-    predicciones_usuario = prediction
+    # predicciones_usuario = prediction
     cant_predicciones = 0
     # Resultados de los partidos por los que aposto
     # resultados = Results.where(prediction_id: gambler.prediction_id)
-    predicciones_usuario.each do |p|
+    prediction.each do |p|
       r = Result.where(match_id: p.match_id).first
       next if r.nil?
 
-      t1GoalsP = p.team1_goals # Goles equipo 1 segun prediccion
-      t2GoalsP = p.team2_goals # Goles equipo 2 segun prediccion
-      t1GoalsR = r.team1_goals # Goles equipo 1 segun resultados
-      t2GoalsR = r.team2_goals # Goles equipo 2 segun resultados
+      # p.team1_goals - Goles equipo 1 segun prediccion
+      # p.team2_goals - Goles equipo 2 segun prediccion
+      # r.team1_goals - Goles equipo 1 segun resultados
+      # r.team2_goals - Goles equipo 2 segun resultados
       # Si la cantidad de goles coincide, se suma un acierto
-      cant_aciertos += 1 if t1GoalsP == t1GoalsR && t2GoalsP == t2GoalsR
+      cant_aciertos += 1 if p.team1_goals == r.team1_goals && p.team2_goals == r.team2_goals
       cant_predicciones += 1
     end
     self.Statistical_data = (cant_aciertos * 100) / cant_predicciones if cant_predicciones != 0
--- src/models/init.rb
+++ src/models/init.rb
@@ -1,4 +1,6 @@
-require_relative 'Gambler'
+# frozen_string_literal: true
+
+require_relative 'gambler'
 require_relative 'Match'
 require_relative 'Prediction'
 require_relative 'Team'
