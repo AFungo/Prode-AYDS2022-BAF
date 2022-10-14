f1acbb85b3be98eda4632919cd3b05b4c5e81159
Valeria-Bettoni <valebettoni.vb@gmail.com>
2022-10-14 13:20:55 -0300

    coreccion comillas dobles a simples

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -11,57 +11,57 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_18_201607) do
-  create_table "gamblers", force: :cascade do |t|
-    t.string "name"
-    t.string "Email"
-    t.integer "Total_score", default: 0
-    t.float "Statistical_data", default: 0.0
-    t.integer "predictions_id"
-    t.boolean "Admin", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest", default: "", null: false
-    t.index ["predictions_id"], name: "index_gamblers_on_predictions_id"
+  create_table 'gamblers', force: :cascade do |t|
+    t.string 'name'
+    t.string 'Email'
+    t.integer 'Total_score', default: 0
+    t.float 'Statistical_data', default: 0.0
+    t.integer 'predictions_id'
+    t.boolean 'Admin', default: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'password_digest', default: '', null: false
+    t.index ['predictions_id'], name: 'index_gamblers_on_predictions_id'
   end
 
-  create_table "matches", force: :cascade do |t|
-    t.integer "local_id"
-    t.integer "visitor_id"
-    t.datetime "datetime"
-    t.integer "round"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["local_id"], name: "index_matches_on_local_id"
-    t.index ["visitor_id"], name: "index_matches_on_visitor_id"
+  create_table 'matches', force: :cascade do |t|
+    t.integer 'local_id'
+    t.integer 'visitor_id'
+    t.datetime 'datetime'
+    t.integer 'round'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['local_id'], name: 'index_matches_on_local_id'
+    t.index ['visitor_id'], name: 'index_matches_on_visitor_id'
   end
 
-  create_table "predictions", force: :cascade do |t|
-    t.integer "gambler_id"
-    t.integer "match_id"
-    t.integer "team1_goals"
-    t.integer "team2_goals"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index "\"gambler\", \"match\"", name: "index_predictions_on_gambler_and_match"
-    t.index ["gambler_id"], name: "index_predictions_on_gambler_id"
-    t.index ["match_id"], name: "index_predictions_on_match_id"
+  create_table 'predictions', force: :cascade do |t|
+    t.integer 'gambler_id'
+    t.integer 'match_id'
+    t.integer 'team1_goals'
+    t.integer 'team2_goals'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index '\'gambler\', \'match\'', name: 'index_predictions_on_gambler_and_match'
+    t.index ['gambler_id'], name: 'index_predictions_on_gambler_id'
+    t.index ['match_id'], name: 'index_predictions_on_match_id'
   end
 
-  create_table "results", force: :cascade do |t|
-    t.integer "match_id"
-    t.integer "prediction_id"
-    t.integer "team1_goals"
-    t.integer "team2_goals"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["match_id"], name: "index_results_on_match_id"
-    t.index ["prediction_id"], name: "index_results_on_prediction_id"
+  create_table 'results', force: :cascade do |t|
+    t.integer 'match_id'
+    t.integer 'prediction_id'
+    t.integer 'team1_goals'
+    t.integer 'team2_goals'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['match_id'], name: 'index_results_on_match_id'
+    t.index ['prediction_id'], name: 'index_results_on_prediction_id'
   end
 
-  create_table "teams", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'teams', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
 end
